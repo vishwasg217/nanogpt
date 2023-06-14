175f5c83ab9129c53972c1386aeecaaa8b92555a
vishwasg217 <vishwas.g217@gmail.com>
2023-06-14 23:15:03 +0530

    first commit

--- script2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# read -p "Enter the start value: " start_value
-nohup uvicorn server:app > uvicorn.log &
-echo "\nServer Started!!\n"
-curl -X POST -H "Content-Type: application/json" -d '{"start": "The"}' http://localhost:8000/generate-text
